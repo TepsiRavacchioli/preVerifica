4df12c19a60ecbc1c0a48a667f2031254e8e2935
TepsiRavacchioli <cristina.ravacchioli@secondarieclago.it>
2024-05-05 23:00:59 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] valori = { 19, 44, 38, 15, 40 };
+            int iMin = 0;
+            for (int i = 0; i <= valori.Length - 1; i++)
+            {
+                for (int j = 0; j <= valori.Length - 1; j++)
+                {
+                    if (valori[i] < valori[j])
+                    {
+                        iMin = valori[j];
+                        valori[j] = valori[i];
+                        valori[i] = iMin;
+                    }
+                }
+            }
+            foreach (int a in valori)
+                Console.WriteLine($"sort 1 valori ordinati:/n{a}");
+        }
+    }
+}
